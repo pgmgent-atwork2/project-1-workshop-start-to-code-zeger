6f4650924ba982809138e473f2ec57cb59c92c2d
pgm-zegerschaeverbeke <zeger.schaeverbeke@student.arteveldehs.be>
2025-04-30 23:14:54 +0200

    Costum cursor color change on hover

--- tutorial/src/scripts/main.js
+++ tutorial/src/scripts/main.js
@@ -3,3 +3,16 @@ document.addEventListener("mousemove", (event) => {
   customCursor.style.left = `${event.clientX}px`;
   customCursor.style.top = `${event.clientY}px`;
 });
+
+const links = document.querySelectorAll("a");
+const customCursor = document.getElementById("custom-cursor");
+
+links.forEach((link) => {
+  link.addEventListener("mouseenter", () => {
+    customCursor.style.backgroundColor = "red";
+  });
+
+  link.addEventListener("mouseleave", () => {
+    customCursor.style.backgroundColor = "var(--cursor-color)";
+  });
+});
