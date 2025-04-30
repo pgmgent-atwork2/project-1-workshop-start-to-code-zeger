7ad1cc39df85809872e6e6787a16cd85a9cb76a6
pgm-zegerschaeverbeke <zeger.schaeverbeke@student.arteveldehs.be>
2025-04-30 21:28:12 +0200

    Basic javascript setup to move ball when clicked and display red when clicking on background

---
+++ demo/src/scripts/main.js
@@ -0,0 +1,51 @@
+function moveBall() {
+  const $ball = document.getElementById('ball');
+
+  const maxX = window.innerWidth - $ball.offsetWidth;
+  const maxY = window.innerHeight - $ball.offsetHeight;
+
+  const randomX = Math.random() * maxX;
+  const randomY = Math.random() * maxY;
+
+  $ball.getAnimations().forEach(animation => animation.cancel());
+
+  const animation = $ball.animate(
+    [
+      { transform: `translate(${randomX}px, ${randomY}px)` }
+    ],
+    { duration: 1000, fill: 'forwards' }
+  );
+
+  animation.onfinish = () => {
+    $ball.style.transform = `translate(${randomX}px, ${randomY}px)`;
+  };
+}
+
+function onBallClick(event) {
+  event.stopPropagation();
+  moveBall();
+
+}
+
+function onBackgroundClick() {
+  const $board = document.getElementById('board');
+  $board.animate(
+    [
+      { backgroundColor: 'red' },
+      { backgroundColor: 'rgb(203, 203, 203)' }
+    ],
+    { duration: 400, easing: 'ease-in-out' }
+  );
+}
+
+function init() {
+  const $ball = document.getElementById('ball');
+  const $board = document.getElementById('board');
+
+  $ball.addEventListener('click', onBallClick);
+  $board.addEventListener('click', onBackgroundClick);
+
+  moveBall();
+}
+
+init();
